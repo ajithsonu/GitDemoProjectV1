3fadd0e9b9a77e68596c461e18099683966a7352
ajitheaswari <ajitheaswari3@gmail.com>
2022-04-26 11:53:56 +0530

    First commit of project

---
+++ src/Testproject1.java
@@ -0,0 +1,16 @@
+
+public class Testproject1 {
+	public static void main(String[] args)
+	{
+		String Hungry = "yes";
+		if(Hungry == "yes")
+		{
+			System.out.println("eat something...");
+		}
+		else
+		{
+			System.out.println("Do work......");
+		}
+	}
+
+}
